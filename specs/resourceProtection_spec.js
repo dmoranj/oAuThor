00638180d12e5cfc916e0fd61f6ce4ae73311544
daniel <dmj@tid.es>
2013-03-20 17:09:39 +0100

    Prepare the tests for testing authorization

--- specs/resourceProtection_spec.js
+++ specs/resourceProtection_spec.js
@@ -4,6 +4,8 @@ var
     mockResource = require("./resourceMock"),
     apps = require("../app"),
     proxies = require("../lib/proxyService"),
+    clients = require("../lib/clientService"),
+    grants = require("../lib/grantService"),
     async = require('async'),
     apply = async.apply,
     series = async.series,
@@ -18,6 +20,26 @@ var
     proxy;
 
 describe("Resource management", function () {
+
+    beforeEach(function (done) {
+        series([
+            mockResource.createMockApp,
+            proxies.create,
+            apps.create,
+            apply(clients.create, REDIRECT_URI, "testApp", "confidential")
+        ], function (err, results) {
+            mockRes = results[0];
+            proxy = results[1];
+            server = results[2];
+            CLIENT_ID = results[3].id;
+            CLIENT_SECRET = results[3].secret;
+
+            grants.add(CLIENT_ID, SCOPE, function (error, results) {
+                done();
+            });
+        });
+    });
+
     describe("When a request for a protected resource arrives", function () {
         beforeEach(function (done) {
             options = {
@@ -29,34 +51,25 @@ describe("Resource management", function () {
                 }
             };
 
-            series([
-                mockResource.createMockApp,
-                proxies.create,
-                apps.create
-            ], function (err, results) {
-                mockRes = results[0];
-                proxy = results[1];
-                server = results[2];
-                done();
-            });
-        });
-
-        afterEach(function (done) {
-            series([
-                apply(apps.close, server),
-                apply(proxies.close, proxy),
-                apply(mockResource.close, mockRes)
-            ], function (err, results) {
-                if (err) {
-                    console.error("There was an error cleaning testing state");
-                }
-
-                done();
-            });
+            done();
         });
 
         it("should reject requests without a valid token");
 
         it("should reject requests with a token without enough scope");
+    });
+
+    afterEach(function (done) {
+        series([
+            apply(apps.close, server),
+            apply(proxies.close, proxy),
+            apply(mockResource.close, mockRes)
+        ], function (err, results) {
+            if (err) {
+                console.error("There was an error cleaning testing state");
+            }
+
+            done();
+        });
     });
 });
